f72e2d42d326d6567fc8d9e9fba2ebcd1f90c69b
Chris Crow and Victoria Oh <victoria@>
2018-08-09 14:24:43 -0700

    Add branching for multiple option for a result.

---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+
+  $("button").click(function(event) {
+
+    var gender = $("select#inputGender").val();
+    var extroversion = $("#inputExtroversion").val();
+    var feeling = $("#inputFeeling").val();
+
+    event.preventDefault();
+
+    $("li").hide();
+
+    if (gender === "m") {
+      if (feeling === "emotional") {
+        if (extroversion === "lowKey") {
+          $("#Cap").show();
+        } else if (extroversion === "partyAnimal") {
+          $("#Thor").show();
+        }
+      } else if (feeling === "logical") {
+        if (extroversion === "lowKey") {
+          $("#Hulk").show();
+        } else if (extroversion === "partyAnimal") {
+          $("#Iron").show();
+        }
+      }
+    } else if (gender === "f" && extroversion === "lowKey" && feeling === "emotional") {
+      $("#Wasp").show();
+    } else if (gender === "f" && extroversion === "partyAnimal" && feeling === "logical") {
+      $("#BW").show();
+    } else if (gender === "f" && extroversion === "partyAnimal" && feeling === "emotional") {s
+      $("#SW").show();
+    } else if (gender === "f" && extroversion === "lowkey" && feeling === "logical") {
+      $("#Gam").show();
+
+    };
+  });
+});
